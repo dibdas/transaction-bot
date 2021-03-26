1fce7ecbc512892f6e5673224499989dc73790a1
dibdas <dibyandu08das@gmail.com>
2021-03-26 14:11:35 +0530

    upadted exter.html.erb

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Account, type: :model do
   describe 'associations' do
-    it { should belong_to(:group) }
+    it { should belong_to(:group).optional }
     it { should belong_to(:user) }
   end
 end
