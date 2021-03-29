8e03f505ec37c0265c2cfdb344582ec2a43c5da5
dibdas <dibyandu08das@gmail.com>
2021-03-29 22:12:56 +0530

    modified config folder

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dtvjxtcjz'
+  config.api_key = '112335122869595'
+  config.api_secret = 'MmSYcla44Afd8yGJwvNtbJB0-60'
+  config.secure = true
+  config.cdn_subdomain = true
+end
