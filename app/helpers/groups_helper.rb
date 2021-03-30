0deeb70cdd7a2c33a128f6bc6d48a0a1b96933d6
dibdas <dibyandu08das@gmail.com>
2021-03-30 19:36:02 +0530

    modified group helper

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,29 @@
 module GroupsHelper
+  def format_group(groups)
+    result = ''
+    groups.each do |t|
+      result << '<div>'
+      result << image_tag(t.icon, style: 'height:10em;')
+      result << '  Group name:   ' << '  ' << link_to(t.name, group_path(t.id)) << '  '
+      result << 'Transaction by:  ' << t.user.username << '    '
+      result << ' Created at: ' << t.created_at.to_formatted_s(:short)
+      result << '</div>'
+    end
+    result.html_safe
+  end
+
+
+
+def show_group(group)
+    result = ''
+    group.each do |t|
+      result << '<div>'
+      result << image_tag(t.group.icon, style: 'height:10em;')
+      result << '  Group name:   ' << '  ' << t.name << '  '
+      result << 'Transaction by:  ' << t.user.username << '    '
+      result << ' Created at: ' << t.created_at.to_formatted_s(:short)
+      result << '</div>'
+    end
+    result.html_safe
+  end
 end
