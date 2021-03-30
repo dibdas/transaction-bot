98681bd74a7f9d941921e27ae9bdf453735e6f50
dibdas <dibyandu08das@gmail.com>
2021-03-31 00:13:29 +0530

    added group_spec

--- spec/features/group_spec.rb
+++ spec/features/group_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Create group' do
-  let(:group) { Group.create(name: 'Bar', icon: 'https://testicons.com/testicon.png', user_id: user.id) }
+  let(:group) { Group.create(name: 'Bar', icon: 'hjgkgbkjl', user_id: user.id) }
 
-  it 'should fail to create a group without name' do
-    ev = Group.create(icon: 'https://testicons.com/testicon.png')
-    expect(ev.persisted?).to eql(false)
+  scenario '' do
+ 
+    visit new_group_path
+
+    click_on 'New Group'
+
+    fill_in 'Name', with: 'Bar'
+    fill_in 'Icon',  with: 'hjgkgbkjl'
+
+    click_on 'Create Group'
+    sleep(3)
+    visit  groups_path
+    expect(page).to have_content('Foo Bar')
   end
 
-  it 'should fail to create a group user' do
-    ev = Group.create(name: 'test_group', icon: 'https://testicons.com/testicon.png')
-    expect(ev.persisted?).to eql(false)
-  end
-
-  it 'should create group' do
-    ev = Group.create(name: 'test_group', icon: 'https://testicons.com/testicon.png', user_id: test_user.id)
-    expect(ev.persisted?).to eql(true)
-  end
 end
