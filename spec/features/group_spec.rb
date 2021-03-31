0206ee6e6dbcb39010a5301d7d8241004e9619c2
dibdas <dibyandu08das@gmail.com>
2021-03-31 22:32:33 +0530

    modfied spec

--- spec/features/group_spec.rb
+++ spec/features/group_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
-has_one_attached :image
-validates :width, presence: true, if: ->(item) { !item.image.attached?}
-end
-it 'is valid if image is attached' do
-file = Rails.root.join('spec', 'support', 'assets', 'shipment_item', 'wine-box.jpeg')
-image = ActiveStorage::Blob.create_after_upload!(
-io: File.open(file, 'rb'),
-filename: 'wine-box.jpeg',
-content_type: 'image/jpeg' # Or figure it out from `name` if you have non-JPEGs
-).signed_id
-shipment_item = ShipmentItem.new(image: image)
-expect(shipment_item.valid?).to eq true
+RSpec.describe 'Create new Group', type: :feature do
+  # before(:each) do
+  # User.create(id: 1, username: 'Foo Bar', email: 'foo@bar.com', password: '12345678')
+  # Group.create(name: 'Bar', icon: 'icon.jpeg', user_id: 1)
+  # end
+  # has_one_attached :icon
+  # validates :width, presence: true, if: ->(item) { !item.icon.attached?}
+
+  it 'is valid if image is attached' do
+    file = Rails.root.join('spec', 'support', 'assets', 'group', 'icon.jpeg')
+    icon = ActiveStorage::Blob.create_after_upload!(
+      io: File.open(file, 'rb'),
+      filename: 'icon.jpeg',
+      content_type: 'icon/jpeg' # Or figure it out from `name` if you have non-JPEGs
+    ).signed_id
+    group = Group.create(name:"music",icon: icon)
+    expect(group.valid?).to eq true
+  end
 end
